64d8ea4df37da885063583daa9bf66e81f5d11f1
ธนบัตร ดวงสอาด <thanabat.d@rtarf.mi.th>
2023-04-12 15:11:30 +0700

    edit vite config for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), new WindiCSS(), svgr()],
-  base: '/skillkamp_happy_kids/',
+  // base: '/skillkamp_happy_kids/',
   server: {
     proxy: {
       '/v1': {
