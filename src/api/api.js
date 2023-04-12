204a52a3b93c147cc1c82d7f3e360ace76b622dc
ธนบัตร ดวงสอาด <thanabat.d@rtarf.mi.th>
2023-04-12 16:35:28 +0700

    fix auto redirect

--- src/api/api.js
+++ src/api/api.js
@@ -18,11 +18,11 @@ axios.interceptors.request.use((config) => {
 axios.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response.status === 401 || error.response.status === 403) {
-      if (window.location.pathname !== '/login') {
-        window.location.href = '/login';
-      }
-    }
+    // if (error.response.status === 401 || error.response.status === 403) {
+    //   if (window.location.pathname !== '/login') {
+    //     window.location.href = '/login';
+    //   }
+    // }
     return Promise.reject(error);
   },
 );
