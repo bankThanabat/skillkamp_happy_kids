c4e8b74a981bc2a98d625c7cf0f56e84b8928575
ธนบัตร ดวงสอาด <thanabat.d@rtarf.mi.th>
2023-04-12 10:12:45 +0700

    bug: fixed addTocart not found item

--- src/components/product/ProductModal.jsx
+++ src/components/product/ProductModal.jsx
@@ -1,65 +1,94 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddtoCart from '../buttons/AddtoCart';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { actionTypes } from '../../store/action.type';
 import { addItemToCart } from '../../api/cartAPI';
+import { getProductsBySKU } from '../../api/productAPI';
 
 const ProductModal = ({ pdata, closeModal }) => {
   const dispatch = useDispatch();
-  const [product, setProduct] = useState({ ...pdata } ?? {});
+  const [product, setProduct] = useState({});
+  const history = useNavigate();
+  const [color, setColor] = useState('');
+  const [size, setSize] = useState('');
 
   const showId = (pdata) => {
     history(`/product-info/${pdata?.sku}`, { state: pdata });
   };
 
-  const history = useNavigate();
-  const [color, setColor] = useState('');
+  const fetchProductBySku = async () => {
+    if (!pdata?.sku) return;
+    try {
+      const fetchedProduct = await getProductsBySKU({ sku: pdata?.sku ?? '' });
+      setProduct(fetchedProduct?.data?.detail?.data?.catalog?.product ?? {});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 
   const addToCart = async () => {
     await addItemToCart({
-      sku: product?.sku ?? '00001',
+      sku: product?.sku,
       name: product?.name ?? '',
       price: product?.price ?? null,
       discountedPrice: product?.discountedPrice ?? null,
       color: color,
-      size: '',
+      size: size,
       qty: product?.qty ?? 1,
     });
   };
 
+  useEffect(() => {
+    fetchProductBySku();
+  }, []);
+
+  useEffect(() => {
+    if (product?.options && product?.options[0]) {
+      setColor(product?.options[0]?.selections[0]?.key ?? '');
+      setSize(product?.options[1]?.selections[0]?.value ?? '');
+    }
+  }, [product]);
+
   return (
     <div className="relative p-6 flex-auto">
       <div className="grid grid-cols-2">
-        <img className="" src={pdata?.media[0]?.url ?? ''} />
+        <img className="" src={product?.media && (product?.media[0]?.fullUrl ?? '')} />
         <div>
           <br></br>
-          <p className="text-[20px]  top-0 left-0 pb-3">{pdata?.name ?? ''}</p>
-          <p className="text-[20px]  top-0 left-0 pb-3">{pdata?.formattedPrice ?? ''}</p>
-          <p className="text-[15px]  top-0 left-0 pb-3">{`sku: ${pdata?.sku ?? ''}`}</p>
+          <p className="text-[20px]  top-0 left-0 pb-3">{product?.name ?? ''}</p>
+          <p className="text-[20px]  top-0 left-0 pb-3">{product?.formattedPrice ?? ''}</p>
+          <p className="text-[15px]  top-0 left-0 pb-3">{`sku: ${product?.sku ?? ''}`}</p>
           <p className="w-full text-[15px]  top-0 left-0 pb-3">
             Color : {color}{' '}
           </p>
-          <div className="pb-3">
-            <input
-              type="radio"
-              className=""
-              onClick={() => setColor('White')}
-            ></input>
-            <label className=""> White</label>
+          <div className="pb-3 mr-2">
+            {product?.options && product?.options[0]?.selections && product?.options[0]?.selections.map((item, index) => (
+              <div key={index}>
+                <input
+                  type="radio"
+                  className=""
+                  name={item?.key ?? ''}
+                  onClick={() => setColor(item?.key ?? '')}
+                />
+                <label className=""> {item?.description ?? ''}</label>
+              </div>
+            ))}
           </div>
           <p className="text-[15px]  top-0 left-0 pb-3">Size</p>
           <div className="pb-3">
             <select
-              name="size"
+              name={product?.options && (product?.options[1]?.key ?? '') || ''}
               id="size"
               className=" border border-gray-300 w-full h-[40px]"
+              onChange={(e) => setSize(e.target.value)}
             >
-              <option value="12">12-18 months</option>
-              <option value="18">18-24 months</option>
-              <option value="2">2 years</option>
-              <option value="3">3 years</option>
+              {product?.options && product?.options[1]?.selections && product?.options[1]?.selections.map((item, index) => (
+                <option key={index} value={item?.value ?? ''}>{item?.description ?? ''}</option>
+
+              ))}
             </select>
           </div>
           <p className="text-[15px]  top-0 left-0 pb-3">Quantity</p>
@@ -73,7 +102,13 @@ const ProductModal = ({ pdata, closeModal }) => {
           </div>
           <div className="bottom-0 pb-3" onClick={async () => {
             await addToCart();
-            dispatch({ type: actionTypes.add_cart_item, payload: product });
+            dispatch({
+              type: actionTypes.add_cart_item, payload: {
+                ...product,
+                color: color,
+                size: size,
+              },
+            });
             closeModal();
           }}>
             <AddtoCart />
